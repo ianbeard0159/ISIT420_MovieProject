daedb835002a7e4918784c0a1b7a1a750492a629
Ian Beard <ianbeard0159@gmail.com>
2022-04-06 19:17:31 -0700

    Added functionality for generating random order entries

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,85 @@
 var express = require('express');
 var router = express.Router();
+var fs = require("fs");
+
+let OrderObject = function (_StoreID, _SalesPersonID, _CdID, _PricePaid, _Date) {
+  this.StoreID = _StoreID;
+  this.SalesPersonID = _SalesPersonID;
+  this.CdID = _CdID;
+  this.PricePaid = _PricePaid;
+  this.Date = _Date;
+}
+
+// Add 5 to 30 minutes to the input date object
+function IncreaseDateValue(_prevDate) {
+  const rndTime = 5 + Math.floor(Math.random() * 26);
+  const returnDate = new Date();
+  returnDate.setMinutes(_prevDate.getMinutes() + rndTime);
+  return returnDate;
+}
+
+// Create a single random order
+function CreateRandomOrder(_date) {
+  // Arrays of valid IDs that can be randomly picked from
+  const validStores = [
+    {id: 98053, employees: [1, 2, 3, 4]},
+    {id: 98007, employees: [5, 6, 7, 8]}, 
+    {id: 98077, employees: [9, 10, 11, 12]}, 
+    {id: 98055, employees: [13, 14, 15, 16]}, 
+    {id: 98011, employees: [17, 18, 19, 20]}, 
+    {id: 98064, employees: [21, 22, 23, 24]}
+  ];
+  const validCdIDs = [123456, 123654, 321456, 321654, 654123, 654321, 543216, 354126, 621453, 623451];
+  
+  // Generate random indexes for choosing random values from arrays
+  const rndStoreIndex = Math.floor(Math.random() * validStores.length);
+  const rndCdIndex = Math.floor(Math.random() * validCdIDs.length);
+  const rndSalesPersonIndex = Math.floor(Math.random() * 4);
+
+  // Generate random values for the new order object
+  const rndStoreId = validStores[rndStoreIndex].id;
+  const rndCdId = validCdIDs[rndCdIndex];
+  const rndSalesPersonId = validStores[rndStoreIndex].employees[rndSalesPersonIndex];
+  const rndPricePaid = 5 + Math.floor(Math.random() * 11);
+
+  return new OrderObject(rndStoreId, rndSalesPersonId, rndCdId, rndPricePaid, _date);
+}
+
+// Generate an array of random entries
+function GenerateEntries(_numEntries) {
+  // Set the initial date to the current date/time
+  let date = new Date();
+  let entries = [];
+
+  // Add a given number of random entries to the array
+  for (let i = 0; i < _numEntries; i++) {
+    entries.push(CreateRandomOrder(date));
+    date = IncreaseDateValue(date);
+  }
+
+  return entries;
+
+}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/Create', function(req, res) {
+  let order = CreateRandomOrder(new Date());
+  console.log(order);
+
+});
+
+router.get('/SubmitOne', function(req, res) {
+  res.send(GenerateEntries(5));
+});
+
+router.get('/Submit500', function(req, res) {
+  let data = JSON.stringify(GenerateEntries(500));
+  fs.writeFileSync('entries.json', data);
+  res.send("500");
 });
 
 module.exports = router;
